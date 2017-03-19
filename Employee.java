877d3d24014f77bc0599a7bfd48b189e3d13e942
iram10 <Iramch2710@yahoo.com>
2017-03-19 21:36:45 +0500

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,17 @@
+import java.utill.Scanner;
+public class Employee
+{
+public static void main(String[]args)
+{
+string e.name;
+double salary;
+int e.id;
+scanner scan=new Scanner(system.in);
+e.name=scan.nextLine();
+scanner scan=new Scanner(system.in);
+salary=scan.nextDouble();
+scanner scan=new Scanner(system.in);
+e.id=scan.nextInt();
+system.out.println("E.name is"+e.name"employee salary is="+salary"employee id is="+e.id);
+}
+}
