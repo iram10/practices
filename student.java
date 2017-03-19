f4f6d56f160e9d8ef75130b3cd26ff5ee95869d2
iram10 <Iramch2710@yahoo.com>
2017-03-19 20:25:06 +0500

    Create student.java

---
+++ student.java
@@ -0,0 +1,14 @@
+import.java.util.Scanner;
+public class Student
+{
+public static void main(string[]args)
+{
+string name;
+int rolno;
+scanner scan=new Scanner(system.in);
+name=scan.nextline();
+scanner scan=new Scanner(system.in);
+rolno=scan.nextInt();
+system.out.println("name="+name+"rolno="+rolno);
+}
+}
