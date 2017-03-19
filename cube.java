06cead68dae1be84fa2d8035834c4d07c4dee5f9
iram10 <Iramch2710@yahoo.com>
2017-03-19 19:43:48 +0500

    Create cube.java

---
+++ cube.java
@@ -0,0 +1,13 @@
+import java.util.scanner;
+import java.util.math;
+public class cube;
+{
+public static void main(String [] args)
+{
+float num;
+double c;
+scanner scan= new scanner(system.in)
+c=math.pow(num, 3);
+system.out.println("cube of num is =" +c);
+}
+}
