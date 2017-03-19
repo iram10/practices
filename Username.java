d0120ced698b22d508defc2840ca3f4e6f9126a7
iram10 <Iramch2710@yahoo.com>
2017-03-19 21:43:32 +0500

    Create Username.java

---
+++ Username.java
@@ -0,0 +1,10 @@
+public class Username
+[
+public static void main(String[]args)
+{
+string firstname,lastname;
+firstname="IRAM";
+lastname="yasmin";
+system.out.println("my name is"+firstname+lastname);
+}
+}
